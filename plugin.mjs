82a0db642ad4a8c66149e617e02a70c41c18857f
Angus Hollands <goosey15@gmail.com>
2026-03-23 17:38:53 +0000

    wip: add gallery demo

--- plugin.mjs
+++ plugin.mjs
@@ -1,5 +1,319 @@
 import { filter } from "unist-util-filter";
 
+const galleryDirective = {
+  name: "gallery",
+  doc: "A filtering directive",
+  run() {
+    return [
+      {
+        type: "grid",
+        children: [
+          {
+            type: "div",
+            class:
+              "flt-domain-climate flt-pkg-intake flt-pkg-xarray flt-pkg-dask",
+            children: [
+              {
+                type: "card",
+                url: "https://projectpythia.org/cesm-lens-aws-cookbook",
+                children: [
+                  {
+                    type: "cardTitle",
+                    children: [
+                      {
+                        type: "text",
+                        value: "CESM LENS on AWS Cookbook",
+                      },
+                    ],
+                  },
+                  {
+                    type: "div",
+                    children: [
+                      {
+                        type: "image",
+                        url: "https://raw.githubusercontent.com/projectpythia/cesm-lens-aws-cookbook/main/thumbnail.png",
+                      },
+                      {
+                        type: "div",
+                        children: [
+                          {
+                            type: "span",
+                            children: [
+                              {
+                                type: "text",
+                                value: "climate",
+                              },
+                            ],
+                            style: {
+                              background: "#7A77B4",
+                              display: "inline-block",
+                              borderRadius: 8,
+                              color: "white",
+                              padding: 5,
+                              margin: 5,
+                            },
+                          },
+                          {
+                            type: "span",
+                            children: [
+                              {
+                                type: "text",
+                                value: "intake-esm",
+                              },
+                            ],
+                            style: {
+                              background: "#B83BC0",
+                              display: "inline-block",
+                              borderRadius: 8,
+                              color: "white",
+                              padding: 5,
+                              margin: 5,
+                            },
+                          },
+                          {
+                            type: "span",
+                            children: [
+                              {
+                                type: "text",
+                                value: "xarray",
+                              },
+                            ],
+                            style: {
+                              background: "#B83BC0",
+                              display: "inline-block",
+                              borderRadius: 8,
+                              color: "white",
+                              padding: 5,
+                              margin: 5,
+                            },
+                          },
+                          {
+                            type: "span",
+                            children: [
+                              {
+                                type: "text",
+                                value: "dask",
+                              },
+                            ],
+                            style: {
+                              background: "#B83BC0",
+                              display: "inline-block",
+                              borderRadius: 8,
+                              color: "white",
+                              padding: 5,
+                              margin: 5,
+                            },
+                          },
+                        ],
+                      },
+                    ],
+                  },
+                ],
+                urlSource: "https://projectpythia.org/cesm-lens-aws-cookbook",
+              },
+            ],
+          },
+          {
+            type: "div",
+            class: "flt-domain-climate flt-pkg-intake flt-pkg-xesmf",
+            children: [
+              {
+                type: "card",
+                url: "https://projectpythia.org/cmip6-cookbook",
+                children: [
+                  {
+                    type: "cardTitle",
+                    children: [
+                      {
+                        type: "text",
+                        value: "CMIP6 Cookbook",
+                      },
+                    ],
+                  },
+                  {
+                    type: "div",
+                    children: [
+                      {
+                        type: "image",
+
+                        url: "https://raw.githubusercontent.com/projectpythia/cmip6-cookbook/main/thumbnail.png",
+                      },
+                      {
+                        type: "div",
+                        children: [
+                          {
+                            type: "span",
+                            children: [
+                              {
+                                type: "text",
+                                value: "climate",
+                              },
+                            ],
+                            style: {
+                              background: "#7A77B4",
+                              display: "inline-block",
+                              borderRadius: 8,
+                              color: "white",
+                              padding: 5,
+                              margin: 5,
+                            },
+                          },
+                          {
+                            type: "span",
+                            children: [
+                              {
+                                type: "text",
+                                value: "intake-esm",
+                              },
+                            ],
+                            style: {
+                              background: "#B83BC0",
+                              display: "inline-block",
+                              borderRadius: 8,
+                              color: "white",
+                              padding: 5,
+                              margin: 5,
+                            },
+                          },
+                          {
+                            type: "span",
+                            children: [
+                              {
+                                type: "text",
+                                value: "xesmf",
+                              },
+                            ],
+                            style: {
+                              background: "#B83BC0",
+                              display: "inline-block",
+                              borderRadius: 8,
+                              color: "white",
+                              padding: 5,
+                              margin: 5,
+                            },
+                          },
+                        ],
+                      },
+                    ],
+                  },
+                ],
+                urlSource: "https://projectpythia.org/cmip6-cookbook",
+              },
+            ],
+          },
+          {
+            type: "div",
+            class:
+              "flt-domain-hrrr flt-domain-aws flt-domain-zarr flt-pkg-xarray",
+            children: [
+              {
+                type: "card",
+                url: "https://projectpythia.org/HRRR-AWS-cookbook",
+                class: "foo",
+                children: [
+                  {
+                    type: "cardTitle",
+                    children: [
+                      {
+                        type: "text",
+                        value: "HRRR-AWS Cookbook",
+                      },
+                    ],
+                  },
+                  {
+                    type: "div",
+                    children: [
+                      {
+                        type: "image",
+
+                        url: "https://raw.githubusercontent.com/projectpythia/HRRR-AWS-cookbook/main/thumbnail.png",
+                      },
+                      {
+                        type: "div",
+                        children: [
+                          {
+                            type: "span",
+                            children: [
+                              {
+                                type: "text",
+                                value: "HRRR model",
+                              },
+                            ],
+                            style: {
+                              background: "#7A77B4",
+                              display: "inline-block",
+                              borderRadius: 8,
+                              color: "white",
+                              padding: 5,
+                              margin: 5,
+                            },
+                          },
+                          {
+                            type: "span",
+                            children: [
+                              {
+                                type: "text",
+                                value: "AWS cloud",
+                              },
+                            ],
+                            style: {
+                              background: "#7A77B4",
+                              display: "inline-block",
+                              borderRadius: 8,
+                              color: "white",
+                              padding: 5,
+                              margin: 5,
+                            },
+                          },
+                          {
+                            type: "span",
+                            children: [
+                              {
+                                type: "text",
+                                value: "zarr",
+                              },
+                            ],
+                            style: {
+                              background: "#7A77B4",
+                              display: "inline-block",
+                              borderRadius: 8,
+                              color: "white",
+                              padding: 5,
+                              margin: 5,
+                            },
+                          },
+                          {
+                            type: "span",
+                            children: [
+                              {
+                                type: "text",
+                                value: "xarray",
+                              },
+                            ],
+                            style: {
+                              background: "#B83BC0",
+                              display: "inline-block",
+                              borderRadius: 8,
+                              color: "white",
+                              padding: 5,
+                              margin: 5,
+                            },
+                          },
+                        ],
+                      },
+                    ],
+                  },
+                ],
+                urlSource: "https://projectpythia.org/HRRR-AWS-cookbook",
+              },
+            ],
+          },
+        ],
+      },
+    ];
+  },
+};
+
 const filterDirective = {
   name: "filterable",
   doc: "A filtering directive",
@@ -15,10 +329,10 @@ const filterDirective = {
     },
   },
   run(data) {
-    const categorical = (data.options.categorical || "")
+    const categorical = (data.options?.categorical || "")
       .split(/\s+/)
       .filter((item) => !!item);
-    const prefix = data.options.prefix || "flt-";
+    const prefix = data.options?.prefix || "flt-";
     return [
       {
         type: "anywidget",
@@ -53,7 +367,7 @@ const searchDirective = {
 
 const plugin = {
   name: "AST enhancer",
-  directives: [filterDirective, searchDirective],
+  directives: [filterDirective, searchDirective, galleryDirective],
 };
 
 export default plugin;
